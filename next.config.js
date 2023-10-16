7c80ea9bed62abbff5a9b4c41cab7b31514ef749
Leorio Nguyen <thanhbinhbk888@gmail.com>
2023-10-16 22:51:43 +0700

    tao trang wedding card development env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const env = process.env;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    basePath: env.NEXT_PUBLIC_BASE_PATH,
+    // assetsPrefix: "https://binh168.github.io/phank_wedding_final/",
+    images: {
+        unoptimized: true,
+    }
+}
 
 module.exports = nextConfig
